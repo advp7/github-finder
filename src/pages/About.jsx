c47763a742f93a3b342753d19a8563138fcd6eb5
Advaith <advaith1601@gmail.com>
2023-11-04 16:17:12 +0530

    init github finder

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="text-6xl mb-4">Github Finder</div>
+      <p>
+        Github Finder project that can search through github API and display
+        profiles
+      </p>
+    </div>
+  );
+}
+
+export default About;
